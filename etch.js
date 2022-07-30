11b87297453efa1cd912ab27b3fc8aca62a67ff2
jepjep-rgb <jefroyalde@gmail.com>
2022-07-30 12:56:38 +0800

    Implement grid hover effect

--- etch.js
+++ etch.js
@@ -1,5 +1,9 @@
 const gridContainer = document.querySelector('.sketchpad');
 
+function changeColor() {
+    this.style.backgroundColor = "black";
+}
+
 function createGrid() {
     let gridLength = 16;
     for(let row = 1; row <= gridLength; row++){
@@ -8,6 +12,7 @@ function createGrid() {
         for(let square = 1; square <= gridLength; square++){
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid');
+            gridItem.addEventListener('mouseover', changeColor);
             rowContainer.appendChild(gridItem);
         }
         gridContainer.appendChild(rowContainer);
