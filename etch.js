8222f7b2e1908efd0eea35187e11c945e3737509
jepjep-rgb <jefroyalde@gmail.com>
2022-07-31 21:36:48 +0800

    Implement setting to change coloring method
    Color mode makes the color of the square turn from white to black,
    and rainbow mode makes it turn from white to a random color. The
    appropriately named buttons makes the coloring method change
    between color mode and rainbow mode.

--- etch.js
+++ etch.js
@@ -1,24 +1,46 @@
 const gridContainer = document.querySelector('.sketchpad');
 const resetButton = document.querySelector('.reset');
+const colorButton = document.querySelector('.color');
+const rainbowButton = document.querySelector('.rainbow');
+
+gridContainer.classList.add('colormode');
+console.log(gridContainer.classList.value);
+
+function switchMode(mode) {
+    console.log(gridContainer.classList.value);
+    if (mode === 'color' && !(gridContainer.classList.contains('colormode'))){
+        gridContainer.classList.add('colormode');
+        gridContainer.classList.remove('rainbowmode');
+    } else if (mode === 'rainbow' && !(gridContainer.classList.contains('rainbowmode'))){
+        gridContainer.classList.add('rainbowmode');
+        gridContainer.classList.remove('colormode');
+    }
+    console.log(gridContainer.classList.value);
+}
 
 function changeColor() {
-    let R = Math.floor(Math.random()*256);
-    let G = Math.floor(Math.random()*256);
-    let B = Math.floor(Math.random()*256);
+    colorMode = gridContainer.classList.contains('colormode');
+    rainbowMode = gridContainer.classList.contains('rainbowmode');
+    if (colorMode){
+        this.style.backgroundColor = 'black';
+    } else if (rainbowMode){
+        let R = Math.floor(Math.random()*256);
+        let G = Math.floor(Math.random()*256);
+        let B = Math.floor(Math.random()*256);
 
-    console.log(this.style.backgroundColor);
-    if (this.style.backgroundColor === ''){
-        this.style.backgroundColor = `rgba(${R},${G},${B},.1)`;
-    } else {
-        let bgColor = window.getComputedStyle(this).backgroundColor;
-        // Create string array composed of the actual rgba values
-        let rgb = bgColor.replace(/^rgba?\(|\s+|\)$/g, '').split(',');
-        
-        for (let i in rgb) rgb[i] = parseFloat(rgb[i]);
-        if (rgb[3] < 1) rgb[3] += .1;
+        if (this.style.backgroundColor === ''){
+            this.style.backgroundColor = `rgba(${R},${G},${B},.1)`;
+        } else {
+            let bgColor = window.getComputedStyle(this).backgroundColor;
+            // Create string array composed of the actual rgba values
+            let rgb = bgColor.replace(/^rgba?\(|\s+|\)$/g, '').split(',');
+            
+            for (let i in rgb) rgb[i] = parseFloat(rgb[i]);
+            if (rgb[3] < 1) rgb[3] += .1;
 
-        this.style.backgroundColor = `rgba(${rgb[0]},${rgb[1]},${rgb[2]},${rgb[3]})`
-    }
+            this.style.backgroundColor = `rgba(${rgb[0]},${rgb[1]},${rgb[2]},${rgb[3]})`
+        }
+    } else alert('ERROR!');
 }
 
 function clearGrid() {
@@ -53,5 +75,7 @@ function createGrid(length = 16) {
 }
 
 resetButton.addEventListener('click', resetGrid);
+colorButton.addEventListener('click', () => switchMode('color'));
+rainbowButton.addEventListener('click', () => switchMode('rainbow'));
 
 createGrid();
